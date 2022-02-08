1350a2e3c439b939f66e69674bfe0e4bdbb44aaf
Michelle <56207268+MichelleGray78@users.noreply.github.com>
2022-02-08 18:10:59 +0000

    updated with saved high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 15, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0  
+        self.score = 0
+        with open ("high_score.txt") as file:
+            self.high_score = file.read()
         self.color("white")
         self.penup()
         self.goto(0, 268)
@@ -14,14 +16,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
         
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
         
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open ("high_score.txt", "w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
         
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
     
